094a09f50c1f68580eb6e23cde0c89981db2825e
ahmed ashraf <ahmed.ashraf.devv@gmail.com>
2022-02-23 21:36:52 +0200

    sync

--- components/invoice.vue
+++ components/invoice.vue
@@ -8,44 +8,57 @@
       :light="true"
       flat
     >
-      <div >
+      <div>
         <v-card-text>
           <v-simple-table>
             <template v-slot:default>
               <thead>
-
                 <tr>
-                <th>
-                   <img
-                    style="height : 80px"
-                    src="~/assets/img/dental.png"
-                    alt=""
-                  >
-                  <p class="invoice__no" v-if="isInvoice">
-                    فاتورة رقم : #{{query.no}}
-                  </p>
-                  <p class="invoice__no" v-else>
-                    طلب رقم : #{{query.no}}
-                  </p>
-              <!-- <p class="block">العميل :{{query.customer_name}} / {{this.query.customer_code}} </p> -->
-<p class="block">
-              التاريخ : {{query.date}}
-            </p>
-            <p class="block">سجل تجاري :{{options.BonMsg3}}</p>
-              <p class="block">بطاقة ضريبية :{{options.BonMsg4}} </p>
-
-              <!-- <p class="block">العميل :{{query.customer_name}} / {{this.query.customer_code}} </p> -->
-              <p class="block">العميل :{{query.customer_name}} / {{query.customer_code}} </p>
-
-
                   
-                </th>
+                    <td style="text-align:center" colspan="5">
+                      <img
+                        style="height : 80px"
+                        src="~/assets/img/dental.png"
+                        alt=""
+                      >
+                    <h2 class="mb-4">{{options.ReportTitle}}</h2>
+                      <p
+                        class="invoice__no"
+                        v-if="isInvoice"
+                      >
+                        فاتورة رقم : #{{query.no}}
+                      </p>
+                      <p
+                        class="invoice__no"
+                        v-else
+                      >
+                        طلب رقم : #{{query.no}}
+                      </p>
+                    </td>
+                  
+                </tr>
+                <tr>
+                 
+                    <td colspan="2">
+                      <p class="block">بطاقة ضريبية :{{options.BonMsg4}} </p>
+                      <p class="block">سجل تجاري :{{options.BonMsg3}}</p>
+                    </td>
+                    <td colspan="3">
+                       <p class="block">
+                        التاريخ : {{query.date}}
+                      </p>
+                      <p class="block">العميل :{{query.customer_name}} / {{query.customer_code}} </p>
+                    </td>
+                  
                 </tr>
                 <tr>
                   <th class="text-right">
                     المسلسل
                   </th>
-                  <th class="text-right" style="min-width:300px">
+                  <th
+                    class="text-right"
+                    style="min-width:300px"
+                  >
                     اسم الصنف
                   </th>
                   <th class="text-right">
@@ -54,7 +67,10 @@
                   <th class="text-right">
                     الكمية
                   </th>
-                  <th class="text-right" v-if="!isInvoice">
+                  <th
+                    class="text-right"
+                    v-if="!isInvoice"
+                  >
                     الكمية بالكرتونة
                   </th>
                   <th class="text-right">
@@ -68,13 +84,33 @@
                   :key="item.Name"
                 >
                   <td class="text-right">{{ index + 1 }}</td>
-                  <td class="text-right" style="min-width:300px">{{ item.ItemName }}</td>
+                  <td
+                    class="text-right"
+                    style="min-width:300px"
+                  >{{ item.ItemName }}</td>
                   <td class="text-right">{{ item.Price }}</td>
                   <td class="text-right">{{ item.Qnt }}</td>
-                  <td class="text-right" v-if="!isInvoice">{{ item.QntAntherUnit }}</td>
+                  <td
+                    class="text-right"
+                    v-if="!isInvoice"
+                  >{{ item.QntAntherUnit }}</td>
                   <td class="text-right">{{ item.Total }}</td>
                 </tr>
               </tbody>
+              <tfoot>
+                <tr>
+                  <td colspan="5">
+                    <p
+                      class="text-center"
+                      style="border: 1px solid;padding 10px; margin-top:20px"
+                    >{{convertTotalToWords}} </p>
+                    <p
+                      class="text-center"
+                      style="border: 1px solid;padding 10px; margin-top:20px"
+                    >{{options.BonMsg5}}</p>
+                  </td>
+                </tr>
+              </tfoot>
             </template>
           </v-simple-table>
         </v-card-text>
@@ -91,33 +127,29 @@
 <script>
 import tafqeet from '@/utils/helpers/Tafqeet.js'
 export default {
-  methods:{
-    tafqeet,
+  methods: {
+    tafqeet
   },
-  computed:{
-    convertTotalToWords:{
+  computed: {
+    convertTotalToWords: {
       get: function () {
-        let total = (this.TotalCash + (this.TotalCash * .14)).toFixed(2)
-          let fractions = total.toString().split('.')
-          let pounds = `${tafqeet(fractions[0])} جنيها`
-          let cents =  typeof fractions[1] == 'undefined' || fractions[1] == '00' ? '' : `و ${tafqeet(fractions[1])} قرشا`
-          let val = `${pounds} ${cents} فقط لا غير`
-          return val
+        let total = (this.TotalCash + this.TotalCash * 0.14).toFixed(2)
+        let fractions = total.toString().split('.')
+        let pounds = `${tafqeet(fractions[0])} جنيها`
+        let cents =
+          typeof fractions[1] == 'undefined' || fractions[1] == '00'
+            ? ''
+            : `و ${tafqeet(fractions[1])} قرشا`
+        let val = `${pounds} ${cents} فقط لا غير`
+        return val
       }
     }
   },
-    props:[
-        'items',
-        'query',
-        'store',
-        'isInvoice',
-        'TotalCash',
-        'options'
-    ]
+  props: ['items', 'query', 'store', 'isInvoice', 'TotalCash', 'options']
 }
 </script>
 <style scoped>
-p{
-  margin:0
+p {
+  margin: 0;
 }
 </style>
